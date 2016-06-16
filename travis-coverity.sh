4815f79d5123746318edc291de59fd64446e8326
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2016-06-16 23:13:57 +0300

    try

---
+++ travis-coverity.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+sudo apt-get update && sudo apt-get install oracle-java8-installer
+java -version
+
+
+./mvnw clean verify -Ptravis -DskipTests | grep -v Download
