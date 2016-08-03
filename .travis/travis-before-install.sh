e4a322dd630d993afddb5c1c6d9e7d3834839754
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2016-08-04 00:33:31 +0300

    ignore error

--- .travis/travis-before-install.sh
+++ .travis/travis-before-install.sh
@@ -18,7 +18,7 @@ set -exu
 
 sudo -E apt-get update
 sudo -E apt-get install -q -y wget
-sudo -E apt-get -q -y --purge remove docker-engine
+sudo -E apt-get -q -y --purge remove docker-engine || :
 sudo -E apt-cache policy docker-engine
 
 ./get-docker-com.sh
