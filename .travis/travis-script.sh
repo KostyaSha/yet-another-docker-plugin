4846565df6952b69c770803a8d50caa85dc392c3
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2016-08-08 02:00:55 +0300

    sdf

--- .travis/travis-script.sh
+++ .travis/travis-script.sh
@@ -23,7 +23,7 @@ if [ "${FAST_BUILD}" == "true" ]; then
     fi
 else
     if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$COVERITY" == "true" ] && [ "$IS_COVERITY_SCAN_BRANCH" = "1" ]; then
-        export COVERITY_SCAN_BUILD_COMMAND="./mvnw verify -Pyad-its,!rerunFailingTests"
+        export COVERITY_SCAN_BUILD_COMMAND="./mvnw verify -Pyad-its,!rerunFailingTests | grep -v Downloading"
         #curl -s "https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh" | bash
         ./.travis/travisci_build_coverity_scan.sh
     else
