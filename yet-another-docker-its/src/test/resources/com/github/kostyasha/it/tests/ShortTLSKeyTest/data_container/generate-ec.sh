b4551c0bc73390a0455d2314c84c66205c38ab20
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2016-08-09 03:30:26 +0300

    Fix "Test connection". Choosable implementation. (#53)
    * Fix "Test connection". Choosable implementation.
    
    - test case exists but doesn't catch by unknown reason
    - allow choosing netty/jersey impl
    
    * cleanup
    
    * don't push to s3

--- yet-another-docker-its/src/test/resources/com/github/kostyasha/it/tests/ShortTLSKeyTest/data_container/generate-ec.sh
+++ yet-another-docker-its/src/test/resources/com/github/kostyasha/it/tests/ShortTLSKeyTest/data_container/generate-ec.sh
@@ -31,7 +31,7 @@ openssl req \
     -key server-key.pem \
     -out server.csr
 
-echo subjectAltName = IP:192.168.99.100,IP:127.0.0.1 > extfile.cnf
+echo subjectAltName = IP:192.168.99.100,IP:192.168.99.101,IP:127.0.0.1 > extfile.cnf
 
 openssl x509 \
     -req \
--- yet-another-docker-plugin/src/main/java/com/github/kostyasha/yad/client/ClientBuilderForConnector.java
+++ yet-another-docker-plugin/src/main/java/com/github/kostyasha/yad/client/ClientBuilderForConnector.java
@@ -7,12 +7,14 @@ import com.cloudbees.plugins.credentials.domains.DomainRequirement;
 import com.github.kostyasha.yad.DockerConnector;
 import com.github.kostyasha.yad.docker_java.com.github.dockerjava.api.DockerClient;
 import com.github.kostyasha.yad.docker_java.com.github.dockerjava.api.command.DockerCmdExecFactory;
-import com.github.kostyasha.yad.docker_java.com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.kostyasha.yad.docker_java.com.github.dockerjava.core.DefaultDockerClientConfig.Builder;
 import com.github.kostyasha.yad.docker_java.com.github.dockerjava.core.DockerClientConfig;
 import com.github.kostyasha.yad.docker_java.com.github.dockerjava.core.DockerClientImpl;
 import com.github.kostyasha.yad.docker_java.com.github.dockerjava.core.KeystoreSSLConfig;
 import com.github.kostyasha.yad.docker_java.com.github.dockerjava.core.SSLConfig;
-import com.github.kostyasha.yad.docker_java.com.github.dockerjava.netty.DockerCmdExecFactoryImpl;
+import com.github.kostyasha.yad.docker_java.com.github.dockerjava.jaxrs.JerseyDockerCmdExecFactory;
+import com.github.kostyasha.yad.docker_java.com.github.dockerjava.netty.NettyDockerCmdExecFactory;
+import com.github.kostyasha.yad.other.ConnectorType;
 import com.github.kostyasha.yad.other.VariableSSLConfig;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import hudson.security.ACL;
@@ -48,8 +50,9 @@ public class ClientBuilderForConnector {
     private DockerCmdExecFactory dockerCmdExecFactory = null;
     private DockerClientConfig clientConfig = null;
     // fallback to builder
-    private DefaultDockerClientConfig.Builder configBuilder = new DefaultDockerClientConfig.Builder();
+    private Builder configBuilder = new Builder();
 
+    private ConnectorType connectorType = null;
 
     private ClientBuilderForConnector() {
     }
@@ -75,11 +78,17 @@ public class ClientBuilderForConnector {
             throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
         LOG.debug("Building connection to docker host '{}'", connector.getServerUrl());
         withCredentials(connector.getCredentialsId());
+        withConnectorType(connector.getConnectorType());
         if (nonNull(connector.getTlsVerify())) {
             configBuilder.withDockerTlsVerify(connector.getTlsVerify());
         } // either it fallback to docker-java default
 
         return forServer(connector.getServerUrl(), connector.getApiVersion());
+    }
+
+    public ClientBuilderForConnector withConnectorType(ConnectorType connectorType) {
+        this.connectorType = connectorType;
+        return this;
     }
 
     /**
@@ -133,6 +142,11 @@ public class ClientBuilderForConnector {
         return this;
     }
 
+    public ClientBuilderForConnector withConfigBuilder(Builder configBuilder) {
+        this.configBuilder = configBuilder;
+        return this;
+    }
+
     public ClientBuilderForConnector withDockerClientConfig(DockerClientConfig clientConfig) {
         this.clientConfig = clientConfig;
         return this;
@@ -149,7 +163,11 @@ public class ClientBuilderForConnector {
             KeyManagementException {
 
         if (isNull(dockerCmdExecFactory)) {
-            dockerCmdExecFactory = new DockerCmdExecFactoryImpl();
+            if (connectorType == ConnectorType.JERSEY) {
+                dockerCmdExecFactory = new JerseyDockerCmdExecFactory();
+            } else {
+                dockerCmdExecFactory = new NettyDockerCmdExecFactory();
+            }
         }
 
         if (isNull(clientConfig)) {
