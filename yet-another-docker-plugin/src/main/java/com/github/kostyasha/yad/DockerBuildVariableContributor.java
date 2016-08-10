76b724faec228d4f1b2d69d890a3bf166b1794ca
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2016-08-10 23:48:50 +0300

    switch to pipeline friendly class

--- yet-another-docker-plugin/src/main/java/com/github/kostyasha/yad/DockerBuildVariableContributor.java
+++ yet-another-docker-plugin/src/main/java/com/github/kostyasha/yad/DockerBuildVariableContributor.java
@@ -1,15 +1,18 @@
 package com.github.kostyasha.yad;
 
 import com.github.kostyasha.yad.docker_java.org.apache.http.client.utils.URIBuilder;
+import hudson.EnvVars;
 import hudson.Extension;
-import hudson.model.BuildVariableContributor;
+import hudson.model.EnvironmentContributor;
 import hudson.model.Executor;
 import hudson.model.Run;
+import hudson.model.TaskListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nonnull;
+import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.Map;
 
 /**
  * Contribute docker related vars in build.
@@ -19,16 +22,18 @@ import java.util.Map;
  * @author Kanstantsin Shautsou
  */
 @Extension
-public class DockerBuildVariableContributor extends BuildVariableContributor {
+public class DockerBuildVariableContributor extends EnvironmentContributor {
     private static final Logger LOG = LoggerFactory.getLogger(DockerBuildVariableContributor.class);
 
     @Override
-    public void buildVariablesFor(Run build, Map<String, String> variables) {
-        final Executor executor = build.getExecutor();
+    public void buildEnvironmentFor(@Nonnull Run run, @Nonnull EnvVars envs, @Nonnull TaskListener listener) throws IOException, InterruptedException {
+        final Executor executor = run.getExecutor();
         if (executor != null && executor.getOwner() instanceof DockerComputer) {
             final DockerComputer dockerComputer = (DockerComputer) executor.getOwner();
-            variables.put("DOCKER_CONTAINER_ID", dockerComputer.getContainerId());
-            variables.put("JENKINS_CLOUD_ID", dockerComputer.getCloudId());
+            listener.getLogger().println("[YAD-PLUGIN] Injecting DOCKER_CONTAINER_ID variable.");
+            envs.put("DOCKER_CONTAINER_ID", dockerComputer.getContainerId());
+            listener.getLogger().println("[YAD-PLUGIN] Injecting JENKINS_CLOUD_ID variable.");
+            envs.put("JENKINS_CLOUD_ID", dockerComputer.getCloudId());
             try {
                 //replace http:// and https:// from docker-java to tcp://
                 final DockerCloud cloud = dockerComputer.getCloud(); // checkfornull
@@ -37,9 +42,11 @@ public class DockerBuildVariableContributor extends BuildVariableContributor {
                     if (!uriBuilder.getScheme().equals("unix")) {
                         uriBuilder.setScheme("tcp");
                     }
-                    variables.put("DOCKER_HOST", uriBuilder.toString());
+                    listener.getLogger().println("[YAD-PLUGIN] DOCKER_HOST variable.");
+                    envs.put("DOCKER_HOST", uriBuilder.toString());
                 }
             } catch (URISyntaxException e) {
+                listener.error("Can't build 'DOCKER_HOST' variable: {}", e.getMessage());
                 LOG.error("Can't build 'DOCKER_HOST' var: {}", e.getMessage());
             }
         }
