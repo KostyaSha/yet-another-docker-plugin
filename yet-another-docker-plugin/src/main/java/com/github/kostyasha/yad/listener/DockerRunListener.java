1c2937c6acde1d48ffd1000c530e9d04d07c4bf7
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2017-01-29 23:32:15 +0300

    Rm unused
    Signed-off-by: Kanstantsin Shautsou <kanstantsin.sha@gmail.com>

--- yet-another-docker-plugin/src/main/java/com/github/kostyasha/yad/listener/DockerRunListener.java
+++ yet-another-docker-plugin/src/main/java/com/github/kostyasha/yad/listener/DockerRunListener.java
@@ -43,27 +43,6 @@ import static java.util.Objects.isNull;
 public class DockerRunListener extends RunListener<Run<?, ?>> {
     private static final Logger LOG = LoggerFactory.getLogger(DockerRunListener.class);
 
-
-//    @Override
-//    public void onInitialize(Run<?, ?> run) {
-//        final DockerLabelAssignmentAction assignmentAction = run.getAction(DockerLabelAssignmentAction.class);
-//        if (isNull(assignmentAction)) {
-//            return;
-//        }
-//        try {
-//            final StreamBuildListener listener = createBuildListener(run);
-//            final DockerClient client = assignmentAction.getConnector().getClient();
-//            final Node node = Jenkins.getInstance().getNode(assignmentAction.getAssignedLabel());
-//            final DockerSlaveSingle dockerSlave = (DockerSlaveSingle) node;
-//            final DockerComputerSingle computer = (DockerComputerSingle) dockerSlave.toComputer();
-//
-//            computer.getLauncher().launch(computer, (TaskListener) listener);
-//        } catch (IOException | InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-
     @Override
     public void onStarted(Run<?, ?> run, TaskListener listener) {
         final DockerLabelAssignmentAction assignmentAction = run.getAction(DockerLabelAssignmentAction.class);
@@ -107,28 +86,5 @@ public class DockerRunListener extends RunListener<Run<?, ?>> {
             listener.error("Can't add Docker fingerprint to run.");
             LOG.error("Can't add fingerprint to run {}", run, e);
         }
-    }
-
-    private StreamBuildListener createBuildListener(@Nonnull Run run) throws IOException, InterruptedException {
-        // don't do buffering so that what's written to the listener
-        // gets reflected to the file immediately, which can then be
-        // served to the browser immediately
-        OutputStream logger = new FileOutputStream(run.getLogFile(), true);
-        Job job = run.getParent();
-
-        // Global log filters
-        for (ConsoleLogFilter filter : ConsoleLogFilter.all()) {
-            logger = filter.decorateLogger(run, logger);
-        }
-
-        // Project specific log filters
-        if (job instanceof BuildableItemWithBuildWrappers && run instanceof AbstractBuild) {
-            BuildableItemWithBuildWrappers biwbw = (BuildableItemWithBuildWrappers) job;
-            for (BuildWrapper bw : biwbw.getBuildWrappersList()) {
-                logger = bw.decorateLogger((AbstractBuild) run, logger);
-            }
-        }
-
-        return new StreamBuildListener(logger, run.getCharset());
     }
 }
