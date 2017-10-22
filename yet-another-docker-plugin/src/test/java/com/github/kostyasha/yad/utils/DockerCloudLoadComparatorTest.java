93a8491b858ec1b8b8f3ea68e400fcdb100d38e0
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2017-10-22 23:49:40 +0300

    use not deprecated runner

--- yet-another-docker-plugin/src/test/java/com/github/kostyasha/yad/utils/DockerCloudLoadComparatorTest.java
+++ yet-another-docker-plugin/src/test/java/com/github/kostyasha/yad/utils/DockerCloudLoadComparatorTest.java
@@ -2,14 +2,12 @@ package com.github.kostyasha.yad.utils;
 
 import com.github.kostyasha.yad.DockerCloud;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
@@ -25,7 +23,6 @@ public class DockerCloudLoadComparatorTest {
     @Mock
     private DockerCloud mockCloudLoad;
 
-
     @Test
     public void testDockerCloudLoadComparison() throws Exception {
         //cloud with no load
@@ -37,15 +34,9 @@ public class DockerCloudLoadComparatorTest {
         List<DockerCloud> sortedClouds = Arrays.asList(mockCloudNoLoad, mockCloudLoad);
 
         //test sorting clouds using the DockerCloudLoadComparator
-        Collections.sort(unsortedClouds, new DockerCloudLoadComparator());
+        unsortedClouds.sort(new DockerCloudLoadComparator());
 
         //unsortedClouds should now be sorted
         assertThat(sortedClouds, is(unsortedClouds));
-    }
-
-    @Test
-    public void testSerialVersionUID() {
-        //simple test for something which is not used but required by findbugs
-        assertThat(DockerCloudLoadComparator.getSerialVersionUID(), is(42L));
     }
 }
