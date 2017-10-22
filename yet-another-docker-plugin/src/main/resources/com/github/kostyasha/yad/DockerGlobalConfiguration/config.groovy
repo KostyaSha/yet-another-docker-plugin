cfd97014038099f4d8b41b5340889dd34f50f6a9
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2017-10-23 00:06:00 +0300

    Add ui

--- yet-another-docker-plugin/src/main/resources/com/github/kostyasha/yad/DockerGlobalConfiguration/config.groovy
+++ yet-another-docker-plugin/src/main/resources/com/github/kostyasha/yad/DockerGlobalConfiguration/config.groovy
@@ -1,2 +1,23 @@
 package com.github.kostyasha.yad.DockerGlobalConfiguration
 
+import static com.github.kostyasha.yad.other.cloudorder.DockerCloudOrder.DockerCloudOrderDescriptor.allDockerCloudOrderDescriptors
+
+f.dropdownList(name: "cloudOrder", title: _("Docker Cloud Provisioning Strategy Order")) {
+    allDockerCloudOrderDescriptors().each { sd ->
+        if (sd != null) {
+            f.dropdownListBlock(value: sd.clazz.name, name: sd.displayName,
+                    selected: instance.cloudOrder == null ?
+                            false : instance.cloudOrder.descriptor.equals(sd),
+                    title:  sd.displayName) {
+                descriptor = sd
+                if (instance.cloudOrder != null && instance.cloudOrder.descriptor.equals(sd)) {
+                    instance = instance.cloudOrder
+                }
+                f.invisibleEntry() {
+                    input(type: "hidden", name: "stapler-class", value: sd.clazz.name)
+                }
+                st.include(from: sd, page: sd.configPage, optional: "true")
+            }
+        }
+    }
+}
