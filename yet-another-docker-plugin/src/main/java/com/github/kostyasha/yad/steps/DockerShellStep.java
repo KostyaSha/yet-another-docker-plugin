3f107377ef6826392c5f66fb4b523ebcd85a41ae
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2017-11-01 19:08:44 +0300

    fix vars, use user for launch script

--- yet-another-docker-plugin/src/main/java/com/github/kostyasha/yad/steps/DockerShellStep.java
+++ yet-another-docker-plugin/src/main/java/com/github/kostyasha/yad/steps/DockerShellStep.java
@@ -140,6 +140,7 @@ public class DockerShellStep extends Builder implements SimpleBuildStep {
             insertLabels(containerConfig, run);
 
             if (nonNull(executorScript)) {
+                containerConfig.withUser("root");
                 containerConfig.withEntrypoint("/bin/sh", "/tmp/executor.sh");
                 containerConfig.withCmd("");
             }
@@ -269,7 +270,7 @@ public class DockerShellStep extends Builder implements SimpleBuildStep {
         // add job vars into shell env vars
         try {
             final List<String> envList = isNull(containerConfig.getEnv()) ?
-                    new ArrayList<>() : Arrays.asList(containerConfig.getEnv());
+                    new ArrayList<>() : new ArrayList<>(Arrays.asList(containerConfig.getEnv()));
 
             final EnvVars environment = getEnvVars(run, listener);
             // maybe something should be escaped
